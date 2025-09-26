033e9a7dc3f28c743a19edef218d99fdee9fa571
SomunsF <zhouzhanfeng123@foxmail.com>
2025-09-26 16:59:13 +0800

    first commit

--- build/car_nav/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zzz/ros2_car/ros2/src/car_nav
-# Build directory: /home/zzz/ros2_car/ros2/build/car_nav
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
